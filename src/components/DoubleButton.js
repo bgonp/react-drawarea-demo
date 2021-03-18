a6a9638e1aa3ee5d1d60eccd450e7730600f29c5
Borja Gonzalez <borja@bgon.es>
2021-03-18 23:04:09 +0100

    DoubleButton component

---
+++ src/components/DoubleButton.js
@@ -0,0 +1,31 @@
+import styles from "../styles/DoubleButton.module.css";
+
+const DoubleButton = ({
+  leftLabel,
+  rightLabel,
+  onLeftClick,
+  onRightClick,
+  background = "",
+}) => {
+  console.log({ background });
+  return (
+    <div className={styles.buttons}>
+      <button
+        className={styles.left}
+        onClick={onLeftClick}
+        style={{ background }}
+      >
+        {leftLabel}
+      </button>
+      <button
+        className={styles.right}
+        onClick={onRightClick}
+        style={{ background }}
+      >
+        {rightLabel}
+      </button>
+    </div>
+  );
+};
+
+export default DoubleButton;
