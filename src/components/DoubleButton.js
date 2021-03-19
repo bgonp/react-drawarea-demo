3993acf6c7101d77457f241aa94ff8ea76acce40
Borja Gonzalez <borja@bgon.es>
2021-03-19 01:06:14 +0100

    Removed console.log

--- src/components/DoubleButton.js
+++ src/components/DoubleButton.js
@@ -6,26 +6,23 @@ const DoubleButton = ({
   onLeftClick,
   onRightClick,
   background = "",
-}) => {
-  console.log({ background });
-  return (
-    <div className={styles.buttons}>
-      <button
-        className={styles.left}
-        onClick={onLeftClick}
-        style={{ background }}
-      >
-        {leftLabel}
-      </button>
-      <button
-        className={styles.right}
-        onClick={onRightClick}
-        style={{ background }}
-      >
-        {rightLabel}
-      </button>
-    </div>
-  );
-};
+}) => (
+  <div className={styles.buttons}>
+    <button
+      className={styles.left}
+      onClick={onLeftClick}
+      style={{ background }}
+    >
+      {leftLabel}
+    </button>
+    <button
+      className={styles.right}
+      onClick={onRightClick}
+      style={{ background }}
+    >
+      {rightLabel}
+    </button>
+  </div>
+);
 
 export default DoubleButton;
